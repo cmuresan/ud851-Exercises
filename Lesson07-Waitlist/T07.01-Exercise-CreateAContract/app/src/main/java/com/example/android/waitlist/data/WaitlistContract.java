c47743a8e8bd1af36f09a80687325de6e1f224e7
cristianmmuresan <cristian@wearewip.com>
2018-01-29 06:25:14 +0200

    T07.01 - CreateAContract fix after running tests

--- Lesson07-Waitlist/T07.01-Exercise-CreateAContract/app/src/main/java/com/example/android/waitlist/data/WaitlistContract.java
+++ Lesson07-Waitlist/T07.01-Exercise-CreateAContract/app/src/main/java/com/example/android/waitlist/data/WaitlistContract.java
@@ -8,7 +8,7 @@ public class WaitlistContract {
     }
 
     // COMPLETED (1) Create an inner class named WaitlistEntry class that implements the BaseColumns interface
-    public static class WaitListEntry implements BaseColumns {
+    public final static class WaitListEntry implements BaseColumns {
         // COMPLETED (2) Inside create a static final members for the table name and each of the db columns
         // TABLE_NAME -> waitlist;
         public static final String TABLE_NAME = "waitlist";
